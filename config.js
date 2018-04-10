2bbaac0a577fa306bd3e91551eea1d00085a5197
Anna Piwnik <a.piwnik@o2.pl>
2018-04-10 15:23:51 +0200

    Start task

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID':'546660650225-sq8pqp1vjeo89tmp2t07u2rlam2ba4cv.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'r8LhIfo1oXsCSbM3Ix-SRbu9',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
